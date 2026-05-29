95b1ac67f36c1eff4d4b7ff31e28fbf5a3a45db9
Sofía Villalba Jiménez <sofiasivillalbajimenez@gmail.com>
2026-05-29 19:56:49 +0200

    Nueva rama: React

--- Conexion-API/resources/dto/ZonaGeograficaDTO.java
+++
@@ -1,9 +0,0 @@
-package com.bancosol.dto;
-
-import lombok.*;
-
-@Data @Builder @NoArgsConstructor @AllArgsConstructor
-public class ZonaGeograficaDTO {
-    private Long id;
-    private String nombre;
-}
