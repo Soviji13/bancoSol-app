971a9dea3a3325b8c8e170ff280fc84b26da5c57
CodeJose21 <josegonzb@gmail.com>
2026-06-14 12:14:21 +0200

    Fix bug tiendas

--- src/main/java/com/bancosol/dao/EntidadColaboradoraRepository.java
+++ src/main/java/com/bancosol/dao/EntidadColaboradoraRepository.java
@@ -1,7 +1,10 @@
 package com.bancosol.dao;
 
 import com.bancosol.entities.EntidadColaboradora;
+import com.bancosol.entities.ResponsableEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,14 @@ import java.util.List;
 public interface EntidadColaboradoraRepository extends JpaRepository<EntidadColaboradora, Long> {
     
     List <EntidadColaboradora> findByCoordinador_Id(Long coordinadorId);
+
+    @Query("""
+        SELECT COUNT(DISTINCT tiendaAsignada.id)
+        FROM ResponsableEntidad responsable
+        JOIN responsable.colaborador colaborador
+        LEFT JOIN colaborador.tiendasAsignadas tiendaAsignada
+        WHERE colaborador.coordinador.id = :coordinadorId
+    """)
+    Long contarTiendasPorCoordinador(@Param("coordinadorId") Long coordinadorId);
+
 }
